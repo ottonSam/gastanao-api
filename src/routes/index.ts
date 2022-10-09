4e55dc710da1835f3a2732c994cd68026fd7c03d
Samuel <ottonsam@gmail.com>
2022-10-09 12:18:56 -0300

    initial commit

---
+++ src/routes/index.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+const router = Router();
+
+// router.use("/categories", categoriesRoutes);
+// router.use("/specifications", specificationsRoutes);
+
+export { router };
