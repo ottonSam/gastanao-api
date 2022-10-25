b61f0051ba9f1921a5e7878ff4a59aefc82e2a0d
Samuel <ottonsam@gmail.com>
2022-10-25 08:31:29 -0300

    correcting the filename

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import multer from "multer";
 
 import uploadConfig from "../config/upload";
-import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserCrontroller";
+import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
 import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
