bdb1fc07624477333c04194cecd6bdfa093674c9
Samuel <ottonsam@gmail.com>
2022-10-25 07:00:09 -0300

    error logs config

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,8 @@
-import express from "express";
+import "reflect-metadata";
+import express, {Request, Response, NextFunction } from "express";
+import "express-async-errors";
+
+import { AppError } from "./errors/AppError";
 import { router } from "./routes";
 
 const app = express();
@@ -6,5 +10,18 @@ const app = express();
 app.use(express.json());
 
 app.use(router);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+        return response.status(err.statusCode).json({
+            message: err.message
+        });
+    }
+
+    return response.status(500).json({
+        status: "error",
+        message: `Internal server error - ${err.message}`
+    });
+});
 
 app.listen(3333, () => console.log('🚀 Server is running bro!\nListening on port 3333'));
