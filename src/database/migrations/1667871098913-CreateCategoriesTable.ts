3f802e4768df16f1e2aa67cd96e63a6626cb8386
Samuel <ottonsam@gmail.com>
2022-11-07 22:56:31 -0300

    create categories table

---
+++ src/database/migrations/1667871098913-CreateCategoriesTable.ts
@@ -0,0 +1,51 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateCategoriesTable1667871098913 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "categories",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name: "name",
+                        type: "varchar"
+                    },
+                    {
+                        name: "type",
+                        type: "varchar"
+                    },
+                    {
+                        name: "user_id",
+                        type: "uuid"
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()"
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default: "now()"
+                    },
+                    {
+                        name: "excluded_at",
+                        type: "timestamp",
+                        isNullable: true
+                    }
+                ]
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("categories");
+    }
+
+}
