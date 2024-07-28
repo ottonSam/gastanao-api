77e4b08552d7de04fd64e423fe75bc23e26530c7
Samuel Otton <ottonsam@gmail.com>
2024-07-28 11:41:29 -0300

    Melhorando o CategoryService

--- src/main/java/br/com/ottonsam/gastanaoapi/service/CategoryService.java
+++ src/main/java/br/com/ottonsam/gastanaoapi/service/CategoryService.java
@@ -7,7 +7,6 @@ import br.com.ottonsam.gastanaoapi.entity.dtos.ResponseCategoryDto;
 import br.com.ottonsam.gastanaoapi.repository.CategoryRepository;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Service;
-import org.springframework.util.Assert;
 
 import java.util.List;
 import java.util.Optional;
@@ -24,65 +23,51 @@ public class CategoryService {
         this.userService = userService;
     }
 
-    private Optional<User> getUserData(JwtAuthenticationToken token) {
+    private User getUserData(JwtAuthenticationToken token) {
         Optional<User> user = Optional.ofNullable(userService.findByUsername(token.getName()));
-        Assert.notNull(user, "User not found");
-        return user;
+        if (user.isPresent()) {
+            return user.get();
+        }
+        throw new IllegalArgumentException("user not found");
     }
 
     public ResponseCategoryDto createCategory(CreateCategoryDto categoryDto, JwtAuthenticationToken token) {
-        Optional<User> user = getUserData(token);
-        if (user.isPresent()) {
-            Category category = new Category(categoryDto, user.get());
-            categoryRepository.save(category);
-
-            return new ResponseCategoryDto(category);
-        }
-        return null;
+        User user = getUserData(token);
+        Category category = new Category(categoryDto, user);
+        categoryRepository.save(category);
+        return new ResponseCategoryDto(category);
     }
 
     public List<ResponseCategoryDto> listAllCategories(JwtAuthenticationToken token) {
-        Optional<User> user = Optional.ofNullable(userService.findByUsername(token.getName()));
-        if (user.isPresent()) {
-            List<Category> categories = categoryRepository.findByUser(user.get());
-
-            return categories.stream().map(ResponseCategoryDto::new).toList();
-        }
-
-        return null;
+        User user = userService.findByUsername(token.getName());
+        List<Category> categories = categoryRepository.findByUser(user);
+        return categories.stream().map(ResponseCategoryDto::new).toList();
     }
 
     public ResponseCategoryDto updateCategory(CreateCategoryDto categoryDto, UUID id, JwtAuthenticationToken token) {
         Optional<Category> category = categoryRepository.findById(id);
         if (category.isPresent()) {
             Category categoryToUpdate = category.get();
-
-            Optional<User> user = Optional.ofNullable(userService.findByUsername(token.getName()));
-            if (user.isPresent()) {
-                Assert.isTrue(categoryToUpdate.getUser().equals(user.get()), "This category does not belong to the user");
-                if (categoryToUpdate.getUser().equals(user.get())) {
-                    categoryToUpdate.setDescription(categoryDto.description());
-                    categoryRepository.save(categoryToUpdate);
-
-                    return new ResponseCategoryDto(categoryToUpdate);
-                }
+            User user = userService.findByUsername(token.getName());
+            if (categoryToUpdate.getUser().equals(user)) {
+                categoryToUpdate.setDescription(categoryDto.description());
+                categoryRepository.save(categoryToUpdate);
+                return new ResponseCategoryDto(categoryToUpdate);
             }
+            throw new IllegalArgumentException("This category does not belong to the user");
         }
-
         return null;
     }
 
     public Boolean deleteCategory(UUID id, JwtAuthenticationToken token) {
         Optional<Category> category = categoryRepository.findById(id);
         if (category.isPresent()) {
-            Optional<User> user = Optional.ofNullable(userService.findByUsername(token.getName()));
-            if (user.isPresent()) {
-                Assert.isTrue(category.get().getUser().equals(user.get()), "This category does not belong to the user");
-                if (category.get().getUser().equals(user.get())) {
-                    categoryRepository.delete(category.get());
-                    return true;
-                }
+            User user = userService.findByUsername(token.getName());
+            if (category.get().getUser().equals(user)) {
+                categoryRepository.delete(category.get());
+                return true;
             }
+            throw new IllegalArgumentException("This category does not belong to the user");
         }
         return false;
     }
